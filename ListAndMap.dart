172062fa87cdba58e2d6840197ae050715266086
Ayush Umrao <umraoayush@gmail.com>
2025-12-17 21:16:07 +0530

    learning List And Map

---
+++ ListAndMap.dart
@@ -0,0 +1,14 @@
+void main() {
+  String name = "Ayush";
+  int age = 50;
+  bool isMale = true;
+  List<String> fruits = ["Apple ", "Grapes", "Banana"];
+  Map<String , dynamic> myData = {
+    "name": "Ayush",
+    "age": 50,
+    "isMale": true,
+    "Fruits" :fruits
+  };
+ print(myData["name"]);
+ print(myData["Fruits"]);
+}
