f7e26e3a84850cd942824ff424627f4c9a599d4b
Ayush Umrao <umraoayush@gmail.com>
2025-12-18 23:41:42 +0530

    Using Static keyword

--- functions.dart
+++ functions.dart
@@ -1,15 +1,11 @@
-void main(){
+void main() {
+//  Add add = Add();
 
-  Add add = Add();
- 
- print(add.sumof2(3, 4));
-
+  print(Add.sumof2(7, 9));
 }
 
 class Add {
-  int sumof2(int n1, int n2){
-
-
-  return n1 + n2;
-}
+ static int sumof2(int n1, int n2) {
+    return n1 + n2;
+  }
 }
