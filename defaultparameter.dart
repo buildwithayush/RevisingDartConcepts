d76e8b6a342967b589e15993c06023b8c4056546
Ayush Umrao <umraoayush@gmail.com>
2025-12-20 08:42:29 +0530

    Using Required

--- defaultparameter.dart
+++ defaultparameter.dart
@@ -1,9 +1,9 @@
 void main(){
-Map ayush = usermap(userClass: 30,age: 56);
+Map ayush = usermap(userClass: 30,age: 56 , name: 'Ayush' , gender: 'Male');
 print(ayush);
 }
 
-Map usermap({String? name , int? age , String? gender , int? userClass} ){
+Map usermap({required String name , required int age , required String gender , required int userClass} ){
     return {
       "name" : name,
       "age" : age,
